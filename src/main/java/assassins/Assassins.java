f98fc2a568efe3f1c8f3f5d8b567b6247874742f
eric oneil <eric.oneil1259@gmail.com>
2024-05-01 00:20:23 -0400

    minor tweaks

--- src/main/java/assassins/Assassins.java
+++ src/main/java/assassins/Assassins.java
@@ -6,7 +6,7 @@ public class Assassins {
   ArrayList<String> player = new ArrayList<String>(); 
   ArrayList<String> target = new ArrayList<String>(); 
   String[] match; 
-  // SAMPLE NAMES 
+
   public Assassins() {
     String rawNames = "alisa topuz Leo tomes Amy griffith Massimo elrefaie William Booth Zain Nawaz Sam Henderson Liam Rohacek Amna yasir madi malloy-gomes Jessica Richardson Nishaan Roshan Emily Ackroyd Alex Wang Kristina Nikitopoulos Bella Bartlett Hajer Allani Sahar Sultani Adam Selim Enna Malhotra Katie Archer Jack Guillemette addison milton sophia mcdonald Michael Johnstone Saif akbar Brianna Varcoe James vosper Allison Kim Sami Gupta Abdul Haddad Bartu Bilgen Bridgett Burton Karim Abadeer Oscar Davies Lauren Heath Ella Carriere Matthew Kozanczyn Owen Luongo Chris Matthews Jack Janavicius Lina Alhamzawi Beatrice Abid Ivey Caldwell Cameron Grandfield JP Lopes Rowan Clements Rahul Patel Mousa Adnan Kate Viaene Ethan Wang Neha Gobin Sienna Coughlin Zahra Saeed Justin Yi Radu Romanovschi vivienne georgiou Ariel Huang Ethan Roberts Matthew Nikkari Amy Wang Selina Mancuso Ken Chen Ricardo Rodriguez Hashem Alqusus Erwin zhang Ryan stevens Sachin Khandelwal Wesley Meynen Cassidy Armitage Sara solano Xavier baie Radu Stefan Jackson Bennitt Tameem Ghadri Adrian vedlin Rahil kochhar Karim Halwani Ashley Jarvis Theo Haag Britney Biney Oliver Baker Sureena Bhatia Hareem Shaikh Twheda Hassan Griffin Gault luca barbeito Tej Sangha Zainab Haroon Damian Desousa";
     String[] namesArr = new String[400];
@@ -49,7 +49,7 @@ public class Assassins {
       if (!p.equals(t) && !found) {
         player.remove(0); 
         target.remove(randTarget); 
-        match[i] = p + "-" +  t; 
+        match[i] = i + 1 + ": " + p + " - " +  t; 
         i++;
       }
     }
